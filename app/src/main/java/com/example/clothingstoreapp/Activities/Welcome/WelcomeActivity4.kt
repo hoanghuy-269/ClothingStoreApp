dd005f7599bd14840d9636a43c50f5bef19c0ae9
Quoc-Thong212 <maiquocthong212@gmail.com>
2025-04-13 15:22:33 +0700

    Thong done Welcome

---
+++ app/src/main/java/com/example/clothingstoreapp/Activities/Welcome/WelcomeActivity4.kt
@@ -0,0 +1,29 @@
+package com.example.clothingstoreapp.Activities.Welcome
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.clothingstoreapp.Activities.TrangChuActivity
+import com.example.clothingstoreapp.R
+import com.example.clothingstoreapp.databinding.Welcome4LayoutBinding
+
+class WelcomeActivity4 : AppCompatActivity() {
+    private lateinit var binding: Welcome4LayoutBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = Welcome4LayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnRight.setOnClickListener {
+            val intent = Intent(this,TrangChuActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right)
+        }
+        binding.btnleft.setOnClickListener {
+            val intent = Intent(this,WelcomeActivity3::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_out_left,R.anim.slide_in_left)
+
+        }
+
+    }
+}

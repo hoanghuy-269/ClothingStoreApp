d5422a371c00ac44ef7da1f0ec4e348c6fc72fbf
hoanghuy-269 <hoanghuy29625@gmail.com>
2025-05-02 10:58:48 +0700

    huy push favorite

--- app/src/main/java/com/example/clothingstoreapp/Adapter/ProductAdapter.kt
+++ app/src/main/java/com/example/clothingstoreapp/Adapter/ProductAdapter.kt
@@ -12,30 +12,54 @@ import com.example.clothingstoreapp.model.Product
 
 class ProductAdapter(
     private val originalList: List<Product>,
-    private val onItemClick: (Product) -> Unit
+    private val favoriteIds: MutableSet<String>,
+    private val onItemClick: (Product) -> Unit,
+    private val onAddFavorite: (String) -> Unit,
+    private val onRemoveFavorite: (String) -> Unit
 ) : RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
 
     private var filteredList: MutableList<Product> = originalList.toMutableList()
 
-    inner class ProductViewHolder(private val binding: ItemProductBinding) :
+    inner class ProductViewHolder(val binding: ItemProductBinding) :
         RecyclerView.ViewHolder(binding.root) {
+
         fun bind(product: Product) {
             binding.txtName.text = product.name
             binding.txtPrice.text = "${product.price} đ"
             binding.txtRating.text = "${product.rating}"
 
             Glide.with(binding.root.context)
-                .load(product.images?.trim())
+                .load(product.images.firstOrNull())
                 .error(R.drawable.img_item_wishlist)
                 .into(binding.imgProduct)
 
-            binding.imgFavorite.setImageResource(R.drawable.ic_heart)
+            // Xử lý trạng thái yêu thích
+            var isFavorite = favoriteIds.contains(product.id)
+            binding.imgFavorite.setImageResource(
+                if (isFavorite) R.drawable.ic_heart_red else R.drawable.ic_heart
+            )
+
+            // Xử lý khi click vào trái tim
+            binding.imgFavorite.setOnClickListener {
+                isFavorite = !isFavorite
+                if (isFavorite) {
+                    onAddFavorite(product.id)
+                    (favoriteIds as MutableSet).add(product.id)
+                } else {
+                    onRemoveFavorite(product.id)
+                    (favoriteIds as MutableSet).remove(product.id)
+                }
+                // Cập nhật lại icon
+                notifyItemChanged(adapterPosition)
+            }
 
             binding.root.setOnClickListener {
                 onItemClick(product)
             }
         }
     }
+
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
         val binding = ItemProductBinding.inflate(LayoutInflater.from(parent.context), parent, false)

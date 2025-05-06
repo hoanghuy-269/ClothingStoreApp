90dbde9c3eee9022bd8c2bb12ee6c5d7edba901f
hoanghuy-269 <hoanghuy29625@gmail.com>
2025-05-07 00:29:00 +0700

    push

--- app/src/main/java/com/example/clothingstoreapp/Model/OrderItem.kt
+++ app/src/main/java/com/example/clothingstoreapp/Model/OrderItem.kt
@@ -2,13 +2,12 @@ package com.example.clothingstoreapp.Model
 
 data class OrderItem(
     val productId: String = "",
-    val productName: String = "",
     var quantity: Int = 1,
-    val price: Double = 0.0,
-    val size: String = ""
+    val price: Double = 0.0
 )
 
 data class Order(
+    val id: String = "",
     val userId: String = "",
     val orderDate: String = "",
     val status: String = "",
